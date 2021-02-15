c693c7463d9a67cc2b053b95df6982f33adb59c0
isaacteh <isaacteh@ust-global.com>
2021-02-15 17:33:12 +0800

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
-import React from 'react'
+import React from "react"
+import { graphql } from "gatsby"
 
-export default () => (
-    <h1>Hello</h1>
-)
+const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
+export const query = graphql`
+  {
+    allShopifyProduct {
+      edges {
+        node {
+          id
+          title
+          handle
+          productType
+          vendor
+          variants {
+            id
+            title
+            price
+          }
+        }
+      }
+    }
+  }
+`
+export default ComponentName
